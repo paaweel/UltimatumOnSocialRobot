9a9cc91726d9d4928a421e0a912dd8c24ffcc7c0
ilonatommy <ili.ili@interia.pl>
2021-03-27 19:30:55 +0100

    Added logging in videoModule

--- videoModule.py
+++ videoModule.py
@@ -10,6 +10,7 @@ from collections import deque
 import zmq
 import time
 from PIL import Image
+import logging
 
 
 class VideoModule:
@@ -18,71 +19,81 @@ class VideoModule:
         self.session = qi.Session()
         try:
             self.session.connect("tcp://" + ip + ":" + port)
-            self.video_service = self.session.service("ALVideoDevice")
-            print("Robot connected to VIDEO module.")
+            logging.debug('Connecting robot to ALVideoDevice service')
+            self.videoService = self.session.service("ALVideoDevice")
+
+            logging.debug('Connecting robot to ALMemory service')
+            self.memoryService = self.session.service("ALMemory")
+
+            logging.debug('Connecting robot to ALFaceDetection service')
+            self.faceDetection = self.session.service("ALFaceDetection")
         except RuntimeError:
-            print("Can't connect to Pepper at ip \""
+            logging.error("Can't connect to Pepper at ip \""
                   + ip + "\" on port " + port + ".\n"
                   + "Please check your script arguments. "
                   + "Run with -h option for help.")
-        self.memory = self.session.service("ALMemory")
-        # Connect the event callback.
-        self.subscriber = self.memory.subscriber("FaceDetected")
-        self.subscriber.signal.connect(self.onHumanTracked)
-        # Get the services ALTextToSpeech and ALFaceDetection.
-        self.face_detection = self.session.service("ALFaceDetection")
-        self.face_detection.subscribe("VideoModule")
-        self.got_face = False
-        self.width = None
-        self.height = None
-
+        logging.debug('Subscribing to a video service')
         self.resolution = 2
         # 0: 0.30, 1, 6, 8: 0.45, 2: 0.6, 7: 0.40, 5: 0.40
         self.colorSpace = 11
         self.fps = 20
-        self.client = self.video_service.subscribeCamera(
-            "python_video" + str(random.random()),
+        self.width = None
+        self.height = None
+        self.client = self.videoService.subscribeCamera(
+            "pythonVideo" + str(random.random()),
             0,
             self.resolution,
             self.colorSpace,
             self.fps
         )
+
+        logging.debug('Subscribing to a memory service.')
+        self.subscriber = self.memoryService.subscriber("FaceDetected")
+        self.subscriber.signal.connect(self.onHumanTracked)
+
+        logging.debug('Subscribing to a face detection service')
+        self.faceDetection.subscribe("VideoModule")
+
         self.seqFrames = deque([], maxlen=3)
         self.emotions = deque([], maxlen=10)
+
+        zmqSocket = "tcp://127.0.0.1:5559"
+        logging.debug('Opening PUSH ZMQ communication on '
+                    + zmqSocket
+                    + 'for facial emotion labels')
         self.context = zmq.Context()
         self.videoEmotionsSocket = self.context.socket(zmq.PUSH)
-        self.videoEmotionsSocket.bind("tcp://127.0.0.1:5559")
+        self.videoEmotionsSocket.bind(zmqSocket)
 
     def closeConnection(self):
-        self.video_service.unsubscribe(self.client)
-        self.face_detection.unsubscribe("VideoModule")
+        logging.debug('Unsubscribing video and face detection services')
+        self.videoService.unsubscribe(self.client)
+        self.faceDetection.unsubscribe("VideoModule")
 
     def onHumanTracked(self, value):
         if value == []:
-            print("onHumanTracked triggered but no face info")
-            self.got_face = False
         else:
-            print("onHumanTracked triggered", value[1])
-            self.got_face = True
+            logging.debug('Human tracked, timestamp: ', value[0])
             # there's an assumption human won't move much between 3 frames
             # if frames too slow - change the cropping error tolerance in cropFace
             for i in range(3):
-                result = self.video_service.getImageRemote(self.client)
+                result = self.videoService.getImageRemote(self.client)
                 if result is None:
-                    print 'cannot capture.'
+                    logging.error('Cannot capture frame')
                 elif result[6] is None:
-                    print 'no image data string.'
+                    logging.error('No image data string')
                 elif self.width == None or self.height == None:
                     self.width = result[0]
                     self.height = result[1]
-                im = np.frombuffer(result[6], np.uint8).reshape(self.height, self.width, 3)
+                im = np.frombuffer(result[6], np.uint8)
+                       .reshape(self.height, self.width, 3)
                 croppedFace = cropFace(im, value[1])
                 self.seqFrames.append(croppedFace)
             # put the 3-frame sequence into AI
-            self.emotions.appendleft(random.randrange(0, 4))
+            predictedEmotion = random.randrange(0, 4)
+            self.emotions.appendleft(predictedEmotion)
             print(self.emotions[0])
             # self.videoEmotionsSocket.send(self.emotions[0])
-            self.got_face = False
 
     def cropFace(self, npImg, faceInfo):
         img = Image.fromarray(npImg)
@@ -106,13 +117,14 @@ class VideoModule:
 
 
 if __name__ == "__main__":
-    print("Starting video service!")
+    logging.basicConfig(filename='videoModule.log',level=logging.DEBUG)
+    logging.debug('Starting video module')
     camera = VideoModule()
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
-        print("Exit signal was sent.")
+        logging.debug('Got KeyboardInterrupt from the user')
 
     camera.closeConnection()
-    print("Video service finished!")
+    logging.debug('Exitting video module')

4f5812fa999a5dce42317a27ad829d70d4632264
ilonatommy <ili.ili@interia.pl>
2021-06-12 11:09:01 +0200

    Features extraction.

--- audioModule.py
+++ audioModule.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import librosa
 import array
 import zmq
 import time
@@ -11,7 +12,6 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from tensorflow.keras.models import model_from_json
 from helperModule import *
-from keras.models import model_from_json
 import sys
 
 
@@ -35,12 +35,41 @@ class AudioModule:
             self.model.load_weights(os.path.join(AI_MODELS_DIR, 'best.hdf5'))
             logging.debug('Model loading finished')
 
-    def analyse():
-        audioFeatures = None
-        predictions = self.model.predict(audioFeatures)
+
+    def extract_features(self, data, sr):
+        # ZCR
+        result = np.array([])
+        zcr = np.mean(librosa.feature.zero_crossing_rate(y=data).T, axis=0)
+        result=np.hstack((result, zcr)) # stacking horizontally
+
+        # Chroma_stft
+        stft = np.abs(librosa.stft(data))
+        chroma_stft = np.mean(librosa.feature.chroma_stft(S=stft, sr=sr).T, axis=0)
+        result = np.hstack((result, chroma_stft)) # stacking horizontally
+
+        # MFCC
+        mfcc = np.mean(librosa.feature.mfcc(y=data, sr=sr).T, axis=0)
+        result = np.hstack((result, mfcc)) # stacking horizontally
+
+        # Root Mean Square Value
+        rms = np.mean(librosa.feature.rms(y=data).T, axis=0)
+        result = np.hstack((result, rms)) # stacking horizontally
+
+        # MelSpectogram
+        mel = np.mean(librosa.feature.melspectrogram(y=data, sr=sr).T, axis=0)
+        result = np.hstack((result, mel)) # stacking horizontally
+
+        return np.expand_dims(np.array(result), axis=-1)
+
+
+    def analyse(self, path):
+        data, sr = librosa.load(path)
+        audioFeatures = self.extract_features(data, sr)
+        logging.debug('Features shape: {0}'.format(audioFeatures.shape))
+        predictions = self.model.predict(np.expand_dims(audioFeatures, axis=0))
         predictedEmotion = np.argmax(predictions)
         print(predictedEmotion, predictions)
-        self.audioEmotionsSocket.send(predictions)
+        # self.audioEmotionsSocket.send(predictions)
         logging.debug('AUDIO: {0} -> {1}'.format(predictedEmotion, predictions))
 
 
@@ -51,6 +80,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         logging.debug('Incorrect number of arguments, required 1 with'\
         'audio file path')
-        return
+        sys.exit(5)
     audioClassifier = AudioModule(str(sys.argv[1]))
+    audioClassifier.analyse(audioClassifier.audioPath)
     logging.debug('Exitting audio module')
